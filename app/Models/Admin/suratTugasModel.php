f2e4285af3bbc510c6ea198eb9c5d6b3c7339602
Nurhidayah <nurhid180701@gmail.com>
2024-10-29 11:57:36 +0700

    nurhidayah

--- app/Models/Admin/suratTugasModel.php
+++ app/Models/Admin/suratTugasModel.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\suratTugasModel;
 use Yajra\DataTables\Facades\DataTables;
 
+// haii aku nurrr
 class SuratTugasModel extends Model
 {
     use HasFactory;
